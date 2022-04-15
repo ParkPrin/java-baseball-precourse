14c1e9a521b869107ec8ed4fe88468bfd53228ef
박종훈 <m05214@naver.com>
2022-04-15 17:12:20 +0900

    Comment : "플레이어 주석 추가"
    플레이어 코드에 주석 추가함

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -5,12 +5,32 @@ import static baseball.common.CommonValidation.threeDigitsNumberOfValidation;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
+
+    /**
+     * 플레이어가 콘솔에 데이터 등록 후 데이터 유효성 검증 후 이상없으면 데이터 반환
+     * @return
+     */
     public String playInputAfterValidationToValue(){
-        final String playInput = Console.readLine();
-        System.out.println(playInput);
+        return playInputAfterValidation(getConsoleValue());
+    }
+
+    /**
+     * 플레이어가 콘솔로 부터 입력한 데이터에 대한 유효성검증
+     * @param playInput
+     * @return
+     */
+    public String playInputAfterValidation(String playInput){
         if (!threeDigitsNumberOfValidation(playInput)){
             throw new IllegalArgumentException("1~9의 서로다른 3자리 숫자를 입력하세요.");
         }
         return playInput;
+    }
+
+    /**
+     * 플레이어가 콘솔에 데이터 입력
+     * @return
+     */
+    public String getConsoleValue() {
+        return Console.readLine();
     }
 }

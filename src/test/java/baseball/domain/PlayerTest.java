72b4e971d94476e1b9e3258c4022c67828504208
박종훈 <m05214@naver.com>
2022-04-15 17:11:15 +0900

    Test : "플레이어 테스트코드 작성"
    플레이어의 데이터 입력, 입력 후 유효성검증에 대한 테스트코드 작성, 콘솔 데이터 입력은 mockito로 대체함, mockito에 대한 의존성 추가

---
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -0,0 +1,60 @@
+package baseball.domain;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class PlayerTest {
+
+    Player player;
+
+    @BeforeEach
+    void setup() {
+        this.player = new Player();
+    }
+
+    @DisplayName("플레이어가 등록한 값이 성공했을 때")
+    @Test
+    void playInputAfterValidationSuccessTest(){
+        // when
+        Player mPlayer = mock(Player.class);
+        when(mPlayer.getConsoleValue()).thenReturn("123");
+        final String s = player.playInputAfterValidation(mPlayer.getConsoleValue());
+        Set<Character> playIntpusCharacters = new HashSet<>();
+        for (char c:  s.toCharArray()) {
+            playIntpusCharacters.add(c);
+        }
+
+        // then
+        assertEquals(3, playIntpusCharacters.size());
+    }
+
+    @DisplayName("플레이어가 등록한 값이 실패했을 때")
+    @ParameterizedTest
+    @CsvSource(value = {"12", "12a", "3475"})
+    void playInputAfterValidationFailTest(String returnValue){
+        // when
+        Player mPlayer = mock(Player.class);
+        when(mPlayer.getConsoleValue()).thenReturn(returnValue);
+        org.assertj.core.api.Assertions.assertThatThrownBy(
+                () -> {
+                    player.playInputAfterValidation(mPlayer.getConsoleValue());
+                }
+        )
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1~9의 서로다른 3자리 숫자를 입력하세요.");
+    }
+
+
+
+}

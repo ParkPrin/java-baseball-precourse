2279c70389e300b85b1a85028528329f8b2d5fc5
박종훈 <m05214@naver.com>
2022-04-15 11:56:37 +0900

    Feat: "Player가 값을 입력하는 로직 구현"
    Player가 3자리의 서로다른 숫자를 입력하고 유효성에 실패하면 IllegalArgumentException 발생시키고, 유효성검증을 통과하면 입력값 반환

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,16 @@
+package baseball.domain;
+
+import static baseball.common.Common.threeDigitsNumberOfValidation;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+    public String playInputAfterValidationToValue(){
+        final String playInput = Console.readLine();
+        System.out.println(playInput);
+        if (!threeDigitsNumberOfValidation(playInput)){
+            throw new IllegalArgumentException("1~9의 서로다른 3자리 숫자를 입력하세요.");
+        }
+        return playInput;
+    }
+}
